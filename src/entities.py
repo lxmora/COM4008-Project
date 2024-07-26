86b4e254a9b402b20aadedf285d28eefcd8e240f
lxmora <loganxmorazain@gmail.com>
2024-07-26 16:08:32 +0100

    fixed object instanciations

--- src/entities.py
+++ src/entities.py
@@ -2,18 +2,18 @@ from constants import *
 import pygame
 
 class Entity():
-    def __init__(self, drawbox : pygame.Rect, collisionbox : pygame.Rect):
+    def __init__(self, drawbox : pygame.Rect, collisionbox : pygame.Rect, image):
         self.drawbox=drawbox
         self.collisionbox=collisionbox
-        self.image=None
+        self.image=image
 
 class Wall(Entity):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, drawbox : pygame.Rect, collisionbox : pygame.Rect, image):
+        super().__init__(drawbox , collisionbox, image)
 
 class Balloon(Entity):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, drawbox : pygame.Rect, collisionbox : pygame.Rect, image):
+        super().__init__(drawbox , collisionbox, image)
         self.velocity=None
 
     def moveLeft():
@@ -21,3 +21,7 @@ class Balloon(Entity):
 
     def moveRight():
         pass
+
+class Obstacle(Entity):
+    def __init__(self, drawbox : pygame.Rect, collisionbox : pygame.Rect, image):
+        super().__init__(drawbox , collisionbox, image)
