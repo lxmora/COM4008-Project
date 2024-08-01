e99467ba7edcc1bd29454d0442a1108d28776a8c
lxmora <loganxmorazain@gmail.com>
2024-08-01 11:52:34 +0100

    Updated Ballon Drawing

--- src/main.py
+++ src/main.py
@@ -7,14 +7,24 @@ screen = pygame.display.set_mode(RESOLUTION)
 clock = pygame.time.Clock()
 running = True
 
+balloon = entities.Balloon(pygame.Rect(BALLOONSTARTPOSITION,BALLOONSIZE),pygame.Rect(BALLOONSTARTPOSITION,BALLOONSIZE),BALLOONSPRITE)
+
 while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    #Collison handling
+
+    #Obstacle Generation
+
+    #Movement handling
 
     screen.fill(BACKGROUNDCOLOR)
+
+    #Entity Drawing
+    screen.blit(balloon.image,balloon.drawbox)
 
     pygame.display.flip()
 
