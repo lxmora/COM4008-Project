cae435b997bbd0c429c9ae0045cfea12af26bcc6
lxmora <loganxmorazain@gmail.com>
2024-08-01 15:20:09 +0100

    Added Spike Sprites to Constants

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,4 @@
 import pygame
-import os.path
 
 GAMESTART = pygame.event.custom_type()
 GAMEEND = pygame.event.custom_type()
@@ -11,4 +10,5 @@ BALLOONHEIGHTOFFSET=28
 BALLOONHEIGHT = RESOLUTION[1]-BALLOONSIZE[1]-BALLOONHEIGHTOFFSET
 BALLOONSTARTPOSITION = ((RESOLUTION[0]/2)-(BALLOONSIZE[0]/2),BALLOONHEIGHT)
 BALLOONSPRITE = pygame.transform.scale(pygame.image.load("assets/balloon.png"),BALLOONSIZE)
+SPIKESPRITES = [pygame.image.load("assets/spike"+str(size)+".png") for size in range(1,6)]
 BALLOONSPEED = 20
