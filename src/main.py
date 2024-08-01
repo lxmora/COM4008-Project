da2f8c4ff0b8a91a2655bf918009df06ba3e5f80
lxmora <loganxmorazain@gmail.com>
2024-08-01 12:28:31 +0100

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -7,7 +7,9 @@ screen = pygame.display.set_mode(RESOLUTION)
 clock = pygame.time.Clock()
 running = True
 
+boundarybox = pygame.Rect((0,0),RESOLUTION)
 balloon = entities.Balloon(pygame.Rect(BALLOONSTARTPOSITION,BALLOONSIZE),pygame.Rect(BALLOONSTARTPOSITION,BALLOONSIZE),BALLOONSPRITE)
+obstacles = []
 
 while running:
 
@@ -17,14 +19,28 @@ while running:
 
     #Collison handling
 
+    for obstacle in obstacles:
+        if obstacle.collisionbox.colliderect(balloon.collisionbox):
+            running = False
+    if not boundarybox.contains(balloon.collisionbox):
+        balloon.velocity *= -1
+
+
     #Obstacle Generation
 
+
+
     #Movement handling
+
+
 
     screen.fill(BACKGROUNDCOLOR)
 
     #Entity Drawing
+
     screen.blit(balloon.image,balloon.drawbox)
+    for obstacle in obstacles:
+        screen.blit(obstacle.image,obstacle.drawbox)
 
     pygame.display.flip()
 
